07e754ac52583906d4336adc68d7d7adb7394a2f
Simon Wotherspoon <Simon.Wotherspoon@aad.gov.au>
2024-05-31 10:56:36 +1000

    Documentation updates

--- R/package.R
+++
@@ -1,9 +0,0 @@
-#' Solar Geolocation for Animal Tracking
-#'
-#' Provides facilities for estimating broadscale animal motions from
-#' local light level data recorded by an archival tag.
-#'
-#' @name SGAT-package
-#' @docType package
-#' @author S. Wotherspoon, M. Sumner, A. Bindoff, B. Raymond and S. Lisovski.
-NULL
